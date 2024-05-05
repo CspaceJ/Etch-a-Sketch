1f8e20ddf736e81c37af1a363d5dfda8dc07b1e4
cj <chirst65@gmail.com>
2024-05-05 19:50:14 -0400

    Basic setup finished

--- scripts.js
+++ scripts.js
@@ -1,12 +1,32 @@
 let gridSize = 16;
 const grid = document.querySelector("#gridContainer");
-console.log(grid.textContent);
 let elementSize = 600 / gridSize;
-for (let i = 0 ; i<(gridSize*gridSize) ; i++){
-    const div = document.createElement("div");
-    div.classList.add(`gridElement`);
-    div.style.width = `${elementSize}px`;
-    div.style.height = `${elementSize}px`;
-    div.addEventListener("mouseout",()=>{div.style.backgroundColor = "black"});
-    grid.appendChild(div);
+let btn = document.querySelector("button");
+
+createGrid();
+
+function resizeGrid(){
+    gridSize = prompt("Please enter a new grid size (1-100)");
+    elementSize = 600/gridSize;
+    removeGrid();
+}
+
+function removeGrid(){
+    const elements = document.querySelectorAll(".gridElement")
+    console.log(elements);
+    for(let element of elements){
+        element.remove();
+    }
+    createGrid();
+}
+
+function createGrid(){
+    for (let i = 0 ; i<(gridSize*gridSize) ; i++){
+        const div = document.createElement("div");
+        div.classList.add(`gridElement`);
+        div.style.width = `${elementSize}px`;
+        div.style.height = `${elementSize}px`;
+        div.addEventListener("mouseout",()=>{div.style.backgroundColor = "black"});
+        grid.appendChild(div);
+    }
 }
